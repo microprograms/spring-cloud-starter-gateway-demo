689e31e4bb86e2915fb388167688f4c0ecdf5d13
xuzewei <microprograms@126.com>
2021-03-02 16:27:33 +0800

    spring-cloud-starter-gateway demo

---
+++ src/main/java/com/github/microprograms/spring_cloud_starter_gateway_demo/SpringCloudStarterGatewayDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.microprograms.spring_cloud_starter_gateway_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStarterGatewayDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudStarterGatewayDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/microprograms/spring_cloud_starter_gateway_demo/SpringCloudStarterGatewayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.microprograms.spring_cloud_starter_gateway_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStarterGatewayDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
